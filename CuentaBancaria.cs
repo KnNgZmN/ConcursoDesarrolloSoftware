6d62366c7585de4e4ba7875dfa1f3cd494711c86
Kevin Guzmán <knngzmn@gmail.com>
2026-03-19 21:07:40 -0500

    Reto 3 - CuentaBancaria

---
+++ CuentaBancaria.cs
@@ -0,0 +1,42 @@
+// Cuenta Bancaria (Encapsulamiento)
+using System;
+class CuentaBancaria
+{
+    private double Saldo;
+
+    public CuentaBancaria(double saldoInicial)
+    {
+        Saldo = saldoInicial;
+    }
+
+    public void Depositar(double cantidad)
+    {
+        if (cantidad > 0)
+        {
+            Saldo += cantidad;
+            Console.WriteLine($"Depositado: {cantidad}. Nuevo saldo: {Saldo}");
+        }
+        else
+        {
+            Console.WriteLine("Cantidad a depositar debe ser mayor a 0.");
+        }
+    }
+    public bool Retirar(double cantidad)
+    {
+        if (cantidad > 0 && cantidad <= Saldo)
+        {
+            Saldo -= cantidad;
+            Console.WriteLine($"Retirado: {cantidad}. Nuevo saldo: {Saldo}");
+            return true;
+        }
+        else
+        {
+            Console.WriteLine("Cantidad a retirar debe ser mayor a 0 y no exceder el saldo disponible");
+            return false;
+        }
+    }
+    public double ObtenerSaldo()
+    {
+        return Saldo;
+    }
+}
